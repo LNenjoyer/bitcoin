066cbe493b9d4c0d97a0552e9034a7ada8c6f6f7
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-05 12:17:02 +0100

    bit

--- bitcoin/MainPage.xaml.cs
+++ bitcoin/MainPage.xaml.cs
@@ -1,25 +1,74 @@
-﻿namespace bitcoin
+﻿using System.Net;
+using System.Text.Json;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace bitcoin
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public class BitcoinRate
+        {
+            public string? code { get; set; }
+            public string? description { get; set; }
+            public double? rate_float { get; set; }
+        }
+        public class BitcoinRate2
+        {
+            public BitcoinRate? USD { get; set; }
+            public BitcoinRate? GBP { get; set; }
+            public BitcoinRate? EUR { get; set; }
+        }
+        public class Bitcoin
+        {
+            public string? chartName { get; set; }
+            public BitcoinRate2 bpi { get; set; }
+        }
+        public class USD
+        {
+            public string? code { get; set; }
+            public IList<Rate> rates { get; set; }
+        }
+        public class Rate
+        {
+            public double? ask { get; set; }
+            public double? bid { get; set; }
+        }
+
 
         public MainPage()
         {
+            double usd = 0;
             InitializeComponent();
-        }
+            string dzis = DateTime.Now.ToString("yyyy-MM-dd");
+            string urlUSD = "https://api.nbp.pl/api/exchangerates/rates/c/usd/" + dzis + "/?format=json";
+            string json;
+            using (var webClient = new WebClient())
+            {
+                json = webClient.DownloadString(urlUSD);
+            }
+            USD dolar = JsonSerializer.Deserialize<USD>(json);
+            usd = (double)dolar.rates[0].ask;
+            //lblUSD.Text = usd.ToString(); 
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            using (var webClient = new WebClient())
+            {
+                json = webClient.DownloadString(url);
+            }
+            Bitcoin bitcoin = JsonSerializer.Deserialize<Bitcoin>(json);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string s = (string)bitcoin.bpi.USD.code + ": ";
+            s += ((double)bitcoin.bpi.USD.rate_float).ToString("# ###.####");
+            lblUSD.Text = s;
+            s = (string)bitcoin.bpi.GBP.code + ": ";
+            s += ((double)bitcoin.bpi.GBP.rate_float).ToString("# ###.####");
+            lblGBP.Text = s;
+            s = (string)bitcoin.bpi.EUR.code + ": ";
+            s += ((double)bitcoin.bpi.EUR.rate_float).ToString("# ###.####");
+            lblEUR.Text = s;
+            s = "PLN: ";
+            s += (usd * (double)bitcoin.bpi.USD.rate_float).ToString("# ###.####");
+            lblPLN.Text = s;
         }
     }
-
 }
